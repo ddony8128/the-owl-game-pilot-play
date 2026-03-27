2c38da10da984b68284360f2177ac4245d1ac634
ddony8128 <ddony0528@gmail.com>
2026-03-27 17:54:06 +0900

    fix

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -112,7 +112,7 @@ export default function IntroPage() {
         router.push("/vote");
         break;
       case "survey":
-        window.open("https://forms.gle/wZ6R4PtJvKWC2CD88", "_blank");
+        window.open("https://forms.gle/dTrfUsKRTuJC3CDr8", "_blank");
         break;
       default:
         break;
